4dabb652ae72ef65b75b8ba26b3a16b3d7916f53
HelenYar <helen.yarosh82@gmail.com>
2021-11-07 12:54:16 +0300

    dj_hw_4_1.2

--- phones/models.py
+++ phones/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Phone(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=60, null=False)
-    image = models.URLField(default=None)
+    image = models.ImageField(default=None)
     price = models.IntegerField(default=None)
     release_date = models.DateField(default=None)
     lte_exists = models.BooleanField(default=None)
